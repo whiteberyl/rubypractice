5f7d1b11b544a52daa3f67ef836e819044243b4b
whiteberyl <47698411+whiteberyl@users.noreply.github.com>
2025-09-25 02:15:34 -0700

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,4 +1,10 @@
 puts "hello from ruby "
+
+#name
 puts "what's ur name?"
 name = gets.chomp
 puts "hello, #{name}!"
+#age
+puts "how old are you?"
+age = gets.chomp
+puts "dang #{age} is pretty old!"
